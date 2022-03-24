760a1643a94c60b21772cbfe8ffa203ae75bdb53
calcruikshank <calcruikshank@gmail.com>
2022-03-24 15:01:04 -0600

    Grid

---
+++ Gamebox/Assets/Scripts/GridClass.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridClass
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+
+    public GridClass(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Testing.singleton.SpawnTile(GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+    public void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+        }
+    }
+}
---
+++ Gamebox/Assets/Scripts/Testing.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] GameObject whiteSquare;
+    [SerializeField] GameObject boostTilePrefab;
+    [SerializeField] GameObject doubleCashTilePrefab;
+    [SerializeField] GameObject autoHarvestTilePrefab;
+
+    public static Testing singleton;
+
+    private void Awake()
+    {
+        if (singleton != null)
+        {
+            Destroy(this);
+        }
+        singleton = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GridClass grid = new GridClass(12, 5, 1, new Vector3(-6, -6, 0));
+
+        GridClass newGrid = new GridClass(12, 5, 1, new Vector3(-6, 1, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnTile(Vector3 spawnPosition)
+    {
+        GameObject newTile = Instantiate(whiteSquare, spawnPosition, Quaternion.identity, this.transform);
+    }
+    public GameObject SpawnBoostTile(Vector3 spawnPosition)
+    {
+        GameObject boostTile = Instantiate(boostTilePrefab, spawnPosition, Quaternion.identity, this.transform);
+        return boostTile;
+    }
+    public GameObject SpawnDoubleCashTile(Vector3 spawnPosition)
+    {
+        GameObject doubleCashTile = Instantiate(doubleCashTilePrefab, spawnPosition, Quaternion.identity, this.transform);
+        return doubleCashTile;
+    }
+    public GameObject SpawnAutoHarvestTile(Vector3 spawnPosition)
+    {
+        GameObject doubleCashTile = Instantiate(autoHarvestTilePrefab, spawnPosition, Quaternion.identity, this.transform);
+        return doubleCashTile;
+    }
+}
